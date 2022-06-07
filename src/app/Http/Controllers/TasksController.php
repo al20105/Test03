24764d07112d3029862867a40605a9e9b4f13d54
hirasatatsuya <omametan1229@gmail.com>
2022-06-07 16:14:47 +0900

    add taskController

--- src/app/Http/Controllers/TasksController.php
+++ src/app/Http/Controllers/TasksController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+// use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\View;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
 
 
 class TasksController extends Controller
 {
+    private $user;
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+
+        $this->middleware(function ($request, $next) {
+            // 認証情報を取得
+
+            $this->user = Auth::user();
+
+            View::share('user', $this->user);
+
+            return $next($request);
+        });
+    }
+    
     public function ShowListWD() { //M3 一覧表示UI処理
         return view('tasks.index');
     }
