ee979d68ed36cdad338247a3c32c548c09688b96
al20014 <al20014@shibaura-it.ac.jp>
2022-06-13 15:23:48 +0900

    Update

---
+++ src/app/Http/Controllers/Task/TaskCheck.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Task;
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+
+trait TaskCheck
+{
+    public function TaskCheck(array $data)
+    {
+        return Validator::make($data, [
+            'task_user' => ['nullable', 'integer'],
+            'name' => ['required', 'string', 'max:100'],
+            'date' => ['required', 'date', 'after_or_equal:now'],
+            'time' => ['required', 'date_format:H:i', ],
+            'memo' => ['nullable', 'string']
+        ]);
+    }
+}
