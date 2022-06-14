bb979113d81fbe42775c3b591e622441baff245a
al20119 <al20119@shibaura-it.ac.jp>
2022-06-14 15:25:19 +0900

    test

--- src/app/Http/Controllers/Task/EditController.php
+++ src/app/Http/Controllers/Task/EditController.php
@@ -2,25 +2,27 @@
 
 namespace App\Http\Controllers\Task;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Contracts\Encryption\DecryptException;
-use Illuminate\Foundation\Auth\RedirectsUsers;
+use App\Models\Task;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Auth\RedirectsUsers;
 
 class EditController extends Controller
 {
     use GetUser;
+    use TaskCheck;
+    use RedirectsUsers;
 
     public function ShowTaskEditWD() { //M6 課題編集画面表示UI処理
         return view('tasks.edit');
     }
 
+    protected $redirectTo = '/tasks';
+
     protected function TaskEdit(Request $request) { //M18 課題編集処理
         $this->TaskCheck($request->all())->validate();
-        $this->user->task()->create($request->all());
+        $this->user->task()->edit($request->all());
 
         return $request->wantsJson() ? new JsonResponse([], 201) : redirect($this->redirectPath());
     }
@@ -28,11 +30,10 @@ class EditController extends Controller
     protected function edit(array $data)
     {
         return Task::edit([
-            'task_user' => $data['task_user'],
+            'task_user' => $this->user->id,
             'name' => $data['name'],
             'date' => $data['date'],
             'time' => $data['time'],
-            'tag' => $data['tag'],
             'memo' => $data['memo'],
         ]);
     }
