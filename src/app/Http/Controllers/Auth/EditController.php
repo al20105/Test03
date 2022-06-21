b02c8394a39c267efeda54146cf00651afb6d9b6
satoshun <al20077@shibaura-it.ac.jp>
2022-06-21 15:46:03 +0900

    アカウント編集２

--- src/app/Http/Controllers/Auth/EditController.php
+++ src/app/Http/Controllers/Auth/EditController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
-use Faker\Guesser\Name;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class EditController extends Controller
 {
 
-    protected $redirectTo = '/edit';
+    protected $redirectTo = '/home';
 
 
     public function ShowEditForm() {
@@ -25,11 +22,15 @@ class EditController extends Controller
         //var_dump($request);
         //$auth = Auth::user();
         $auth = User::find(1);
-        //$this->auth_validate($request->all())->validate(); 
-        $this->update($request->all(),$auth);  
-        //$auth->save($request->all());//追加
-        return view('auth.edit',[ 'auth' =>  $auth]);
-        
+        $this->auth_validate($request->all())->validate();
+        //$comfirm_pass = $request['password'];
+        /*if(Hash::check($comfirm_pass,$auth->password)){
+            $this->update($request->all(),$auth);
+        }else{
+        }
+        */
+        $this->update($request->all(),$auth);
+        return redirect('home');
     }
 
     public function update(array $data, User $user){
@@ -44,33 +45,9 @@ class EditController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            //'email' => ['required', 'string', 'email', 'max:255']
-            //'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            //'password' => ['required', 'string', 'min:8']
         ]);
     }
-/*
-    //userデータの取得
-    public function index() {
-        return view('user.index', ['user' => Auth::user() ]);
-    }
 
-    //userデータの編集
-    public function edit() {
-        return view('auth.edit', ['user' => Auth::user() ]);
-    }
-
-
-    public function update(Request $request) {
-
-        $user_form = $request->all();
-        $user = Auth::user();
-        //不要な「_token」の削除
-        unset($user_form['_token']);
-        //保存
-        $user->fill($user_form)->save();
-        //リダイレクト
-        return redirect('user/index');
-    }
-
-*/
 }
