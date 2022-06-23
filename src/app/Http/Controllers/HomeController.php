7f3435d8f9591d4915acf75ba3a50f5b9894b7c0
al20014 <al20014@shibaura-it.ac.jp>
2022-06-23 18:03:08 +0900

    UPDATE

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
@@ -14,9 +16,16 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware(function ($request, $next) {
+            // 認証情報を取得
+            $this->user = Auth::user();
+            View::share('user', $this->user);
+            return $next($request);
+        });
     }
 
     public function index() {
-        return view('home');
+        $tasks = $this->user->tasks;
+        return view('tasks.index',compact('tasks'));
     }
 }
