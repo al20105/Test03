0e7e03bfa937ef61cde40908cf903e2566cad98a
hirasatatsuya <omametan1229@gmail.com>
2022-06-28 14:50:05 +0900

    アカウント編集処理時エラー解決@平佐

--- src/app/Http/Controllers/Auth/EditController.php
+++ src/app/Http/Controllers/Auth/EditController.php
@@ -1,4 +1,18 @@
 <?php
+/*******************************************************************
+***  File Name		: EditController.php
+***  Version		: V2.0
+***  Designer		: 佐藤　駿介
+***  Date			: 2022.06.25
+***  Purpose       	: アカウント情報を編集する
+***
+*******************************************************************/
+/*
+*** Revision :
+*** V1.0 : 佐藤　駿介, 2022.06.18
+*** V2.0 : 佐藤　駿介, 2022.06.25 アカウント編集処理
+
+*/
 
 namespace App\Http\Controllers\Auth;
 
@@ -7,44 +21,42 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use App\Providers\RouteServiceProvider;
 
 class EditController extends Controller
 {
-    protected $redirectTo = '/home';
-
-    public function ShowEditForm() {
+    public function ShowEditForm() 
+    {
         return view('auth.edit');
     }
 
-    public function userEdit(Request $request) {
-
-        $auth = User::find(1);
+    public function userEdit(Request $request) 
+    {
+        $auth = User::find($request->id);
+        //バリデーション処理
         $this->auth_validate($request->all())->validate();
-        //$comfirm_pass = $request['password'];
-        /*if(Hash::check($comfirm_pass,$auth->password)){
-            $this->update($request->all(),$auth);
-        }else{
-        }
-        */
+        //データの上書き編集
         $this->update($request->all(),$auth);
-        return redirect('home');
+        return redirect('/tasks');
     }
 
-    public function update(array $data, User $user){
-       
+    public function update(array $data, //入力データ
+                            User $user )//ユーザデータ
+    {
+        //データの上書き編集
         return $user->update([
-            'name' => $data['name'],
-            'email' => $data['email']
+            'name' => $data['name'],//ユーザ名
+            'email' => $data['email']//メールアドレス
         ]);
     }
     
     protected function auth_validate(array $data)
     {
+        //バリデーション処理
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255'],
-            //'password' => ['required', 'string', 'min:8']
+            'name' => ['required', 'string', 'max:255'],//ユーザ名
+            'email' => ['required', 'string', 'email', 'max:255'],//メールアドレス
+            'password' => ['required', 'string', 'min:8','current_password']//現在のパスワード
         ]);
     }
-
 }
