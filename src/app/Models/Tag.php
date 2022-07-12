f7e35043849b76b9a5246ee1646939cdc25de44f
al20014 <103486487+al20014@users.noreply.github.com>
2022-07-12 14:48:16 +0900

    Akiba workspace (#37)
    * 受入テストの修正
    
    * コメントを挿入、TaskDelete関連の名称の変更
    
    * up
    
    * up
    
    * Controllerの表題を挿入
    
    * 挿入位置を変更
    
    * memoにnullableを追加
    
    * タグに文字数制限を導入、'<', '>'もはじくように変更
    
    * emailにuniqueのバリデーションを追加
    
    * up
    
    * 細かな修正
    
    * コメントの挿入等
    
    Co-authored-by: Hirasa Tatsuya <103485346+al20105@users.noreply.github.com>

--- src/app/Models/Tag.php
+++ src/app/Models/Tag.php
@@ -1,5 +1,18 @@
 <?php
 
+/*******************************************************************
+*** File Name           : Tag.php
+*** Version             : V1.0
+*** Designer            : 秋葉 星輝
+*** Date                : 2022.07.04
+*** Purpose             : タグの構造体を定義する。
+***
+*******************************************************************/
+/*
+*** Revision :
+*** V1.0 : 秋葉 星輝, 2022.07.04
+*/
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,6 +24,7 @@ class Tag extends Model
 
     protected $fillable = ['name'];
 
+    // 課題の取得
     public function tasks()
     {
         return $this->belongsToMany('App\Models\Task');
--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -1,5 +1,18 @@
 <?php
 
+/*******************************************************************
+*** File Name           : Task.php
+*** Version             : V1.0
+*** Designer            : 平佐 竜也
+*** Date                : 2022.06.14
+*** Purpose             : 課題の構造体を定義する。
+***
+*******************************************************************/
+/*
+*** Revision :
+*** V1.0 : 平佐 竜也, 2022.06.14
+*/
+
 namespace App\Models;          
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,11 +24,13 @@ class Task extends Model
     
     protected $fillable = ['user_id', 'name', 'date', 'time', 'memo'];
 
+    // 親ユーザーの取得
     public function user()
     {
         return $this->belongsTo('App\Models\User');
     }
 
+    // タグの取得
     public function tags()
     {
         return $this->belongsToMany('App\Models\Tag');
