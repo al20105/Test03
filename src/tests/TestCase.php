b60eb842dd1ece82ea968e75cd1fc296b28e22a3
al20119 <al20119@shibaura-it.ac.jp>
2022-07-17 20:54:15 +0900

    コメント9

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -1,10 +1,23 @@
 <?php
 
+/*******************************************************************
+*** File Name           : TestCase.php
+*** Version             : V1.0
+*** Designer            : なし(デフォルト)
+*** Date                : 2022.06.28
+*** Purpose             : テストケース設定
+***
+*******************************************************************/
+/*
+*** Revision :
+*** V1.0 : なし(デフォルト), 2022.06.28
+*/
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication; // 継承
 }
